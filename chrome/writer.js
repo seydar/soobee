e865341cb1e56b2ec5cba1346ee369590ff3398d
Ari Brown <ari@aribrown.com>
2013-01-19 02:30:44 -0500

    trying desperately to toggle

--- chrome/writer.js
+++ chrome/writer.js
@@ -1,9 +1,8 @@
-include("jquery-1.9.0.min.js");
-
-alert('hello');
-
 $(document).mouseup(function(event) {
   var clickedElement = event.target;
-  alert(clickedElement);
+  $(clickedElement).toggle(
+    function() { $(clickedElement).css({"border": "2px solid red"}); },
+    function() { $(clickedElement).css({"border": ""}); }
+  );
 });
 
