f98ca97e2261b6c95ce22dd4e59ded343a9bf79e
Ari Brown <ari@aribrown.com>
2013-01-19 02:39:49 -0500

    finally, i can toggle borders

--- chrome/writer.js
+++ chrome/writer.js
@@ -1,8 +1,5 @@
 $(document).mouseup(function(event) {
   var clickedElement = event.target;
-  $(clickedElement).toggle(
-    function() { $(clickedElement).css({"border": "2px solid red"}); },
-    function() { $(clickedElement).css({"border": ""}); }
-  );
+  $(clickedElement).toggleClass('togglingBorder');
 });
 
